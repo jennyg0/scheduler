fe2235cf03123589ef077cb83f8ce84b54ba21cc
jennyg0 <jen2348@hotmail.com>
2020-10-27 21:12:58 -0400

    add state to form

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -7,13 +7,14 @@ const classNames = require('classnames');
 
 export default function InterviewerList(props) {
   const interviewerList = props.interviewers.map(interviewer => {
+    
     return (
       <InterviewerListItem
         key={interviewer.id}
         name={interviewer.name}
         avatar={interviewer.avatar}
         selected={interviewer.id === props.value}
-        setInterviewer={(event) => props.onChange(interviewer.id)}
+        setInterviewer={() => props.onChange(interviewer.id)}
       />
      )
   })
